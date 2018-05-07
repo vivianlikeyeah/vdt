be4c7e685ac3392c70e48e5b4e0520f8cabbf424
vivianlikeyeah <vivian.bakiris@hotmail.com>
2018-05-07 13:59:00 +1000

    Add files via upload

--- VDTv1.0.py
+++ VDTv1.0.py
@@ -17,6 +17,7 @@ import SimpleITK as sitk
 import sys
 import os
 from math import pi
+from numpy import diff
 
 import glob
 
@@ -24,6 +25,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import re 
+metrics = list() 
+# A list of all the metric values obtained from the image registration 
+images = list()
+# A list of the names of all the images developed in the denoising process 
 
 
 types = {'png': 'png', 'tiff': 'tif', 'jpeg': 'jpg'}
@@ -34,7 +39,13 @@ def input(filename):
 	print("You would like to denoise " + filename)	
 	
 	here = os.path.dirname(os.path.abspath(__file__))
+
+	if args.folder: 
+		here = here + "/" + args.folder
+
+
 	filepath = here + "/" + filename 
+
 	filetype = imghdr.what(filepath)
 	X = io.imread(filepath)
 
@@ -52,7 +63,70 @@ def input(filename):
 
 	return X 
 
+def command_iteration(method) :
+    #if (method.GetOptimizerIteration()==0):
+    #    print("Scales: ", method.GetOptimizerScales())
+    #print("{0:3} = {1:7.5f} : {2}".format(method.GetOptimizerIteration(),
+                                           #method.GetMetricValue(),
+                                           #method.GetOptimizerPosition()))
+	pass
 
+
+def register(moving, fixed): 
+
+	start = time.time()
+
+
+
+
+	here = os.path.dirname(os.path.abspath(__file__))
+	filepath = here + "/" + str(fixed)
+	fixed = sitk.ReadImage(filepath, sitk.sitkFloat32)
+
+	print(filepath)
+
+
+	filepath2 = here + "/" + str(moving)
+	moving = sitk.ReadImage(filepath2, sitk.sitkFloat32)
+	print(filepath2)
+
+
+	#fixed = fixed
+	R = sitk.ImageRegistrationMethod()
+	R.SetMetricAsMattesMutualInformation(numberOfHistogramBins = 50)
+	
+	sample_per_axis=12
+	if fixed.GetDimension() == 2:
+		tx = sitk.Euler2DTransform()
+		# Set the number of samples (radius) in each dimension, with a
+		# default step size of 1.0
+		R.SetOptimizerAsExhaustive([sample_per_axis//2,0,0])
+		# Utilize the scale to set the step size for each dimension
+		R.SetOptimizerScales([2.0*pi/sample_per_axis, 1.0,1.0])
+	elif fixed.GetDimension() == 3:
+		tx = sitk.Euler3DTransform()
+		R.SetOptimizerAsExhaustive([sample_per_axis//2,sample_per_axis//2,sample_per_axis//4,0,0,0])
+		R.SetOptimizerScales([2.0*pi/sample_per_axis,2.0*pi/sample_per_axis,2.0*pi/sample_per_axis,1.0,1.0,1.0])
+
+	# Initialize the transform with a translation and the center o
+	# rotation from the moments of intensity
+
+	tx = sitk.CenteredTransformInitializer(moving, fixed, tx)
+	R.SetInitialTransform(tx)
+	R.SetInterpolator(sitk.sitkLinear)
+	#R.AddCommand( sitk.sitkIterationEvent, lambda: command_iteration(R) )
+	outTx = R.Execute(moving,fixed)
+	#print("-------")
+	#print(outTx)
+	#print("Optimizer stop condition: {0}".format(R.GetOptimizerStopConditionDescription()))
+	#print(" Iteration: {0}".format(R.GetOptimizerIteration()))
+	#print(" Metric value: {0}".format(R.GetMetricValue()))
+	metrics.append(abs(R.GetMetricValue()))
+	print(abs(R.GetMetricValue()))
+
+	end = time.time()
+
+	print('Time to Register ' + str(end-start))
 
 
 #Denoising 
@@ -62,36 +136,113 @@ def denoise (inputFile):
 
 	start = time.time()
 
-	F = ptv.tv1_2d(inputFile, args.lamb, 1, 3)
+	F = ptv.tv1_2d(inputFile, args.lamb,1,3)
 
 	end = time.time()
 
-	print('Time elapsed ' + str(end-start))
+	print('Time to denoise ' + str(end-start))
 	return F 
 
 def denoiseManual(inputFile,lamb):
 
 	start = time.time()
 
-	F = ptv.tv1_2d(inputFile, lamb, 1, 3)
+	F = ptv.tv1_2d(inputFile, lamb,1 ,3)
 
 	end = time.time()
 
 	print('Time elapsed ' + str(end-start))
+
 	return F 
 
 def denoiseAutomated(inputFile):
 	print("Start Automated Denoising")
 
 	fileToDenoise = inputFile
+	previousFile = inputFile
+	original = "hello"
 
-	for x in np.arange(0,0.5,0.001):
+	for x in np.arange(0,0.12,0.005):
+
 		print x
+
 		step = denoiseManual(fileToDenoise,x)
 		name = "Oddstep" + str(x) + ".png"
+		#showFile(step)
 		io.imsave(str(name), step)
-		fileToDenoise = step 
+
+		#here = os.path.dirname(os.path.abspath(__file__))
+		#filepath = here + "/" + str(name)
+		#X = io.imread(filepath)
+		#X = ski.img_as_float(X)
+		
+		#fileToDenoise = X
+		#fileToDenoise = step
+
+		images.append(name)
+		if x == 0:
+			original = name 
+
+		register(name, original)
+
+
+	#calculateBest(metrics)
 		#print x
+
+def denoiseAutomatedPairWise(inputFile):
+	print("Start Automated Denoising")
+
+	fileToDenoise = inputFile
+	increment = 0.005
+	prevx = 0.0 
+
+	for x in np.arange(0,0.12,increment):
+
+		print("The x value is")
+		
+		print x
+
+		step = denoiseManual(fileToDenoise,x)
+		current = "Oddstep" + str(x) + ".png"
+
+
+
+		previous = "Oddstep" + str(prevx) + ".png"
+
+
+
+		#showFile(step)
+		print("Saving the file name", str(current))
+		io.imsave(str(current), step)
+		images.append(current)
+
+		register(current, previous)
+		prevx = x 
+		#print x
+
+def calculateBest():
+
+
+	dif=np.diff(metrics)
+	dif2=np.diff(dif)
+	#plt.plot(dif)
+	#plt.plot(dif2)
+	#plt.show()
+	
+	fn=True
+	for k,i in enumerate(dif2):
+		if i<0:
+			if not fn:
+				print(k+1)
+				if ((k+1) > 5):
+					break
+
+			fn=False
+
+	print (k+1)
+
+	return (k+1)
+
 
 
 def showFile(file):
@@ -105,23 +256,27 @@ def showFile(file):
 		warnings.simplefilter("ignore")
 		out = img_as_uint(file)
 	
+	if args.file: 
 	
-	outname = outfileName()
+		outname = outfileName(args.file)
+	else: 
+		outname = "hello.png"
 
 	#You need to save the image you want to see 
 
 	io.imsave(str(outname), out)
 	
 
-
+	name = "showFile" #+ str(number)
 	img = cv2.imread(str(outname),0)
 	imS = cv2.resize(img, (700, 760))  
-	cv2.namedWindow('showfile', cv2.WINDOW_NORMAL)
-	cv2.moveWindow('showfile', 05,20);
-	cv2.imshow('showfile',imS)
-	#cv2.waitKey(0) #closes after a key press 
-	#cv2.destroyAllWindows()
+	cv2.namedWindow(name, cv2.WINDOW_NORMAL)
+	cv2.moveWindow(name, 05,20);
+	cv2.imshow(name,imS)
 
+	cv2.waitKey(0) #closes after a key press 
+	cv2.destroyAllWindows()
+	'''
 	imgOriginal = cv2.imread(args.file,0)
 	imOriginal = cv2.resize(imgOriginal,(700,760))
 	cv2.namedWindow('original image', cv2.WINDOW_NORMAL)
@@ -129,7 +284,7 @@ def showFile(file):
 	cv2.imshow('original image', imOriginal)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
-
+	'''
 
 	#image = Image.open("tempfile.png")
 	#image.show()
@@ -142,6 +297,31 @@ def showFile(file):
 	#	fig.add_subplot(rows, columns, i)
 	#	io.imshow(file)
 	#plt.show()
+
+
+def openFile(filename):
+
+	print("Open file function!")
+	
+
+	name = "Denoised File" #+ str(number)
+	img = cv2.imread(filename,0)
+	imS = cv2.resize(img, (700, 760))  
+	cv2.namedWindow(name, cv2.WINDOW_NORMAL)
+	cv2.moveWindow(name, 05,20);
+	cv2.imshow(name,imS)
+	#cv2.waitKey(0) #closes after a key press 
+	#cv2.destroyAllWindows()
+
+	imgOriginal = cv2.imread(args.file,0)
+	imOriginal = cv2.resize(imgOriginal,(700,760))
+	cv2.namedWindow('original image', cv2.WINDOW_NORMAL)
+	cv2.moveWindow('original image', 710,20)
+	cv2.imshow('original image', imOriginal)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+
 
 def denoiseManualIteration(inputFile,lamb):
 
@@ -218,10 +398,10 @@ def output(outputFile):
 
 
 
-def outfileName():
+def outfileName(file):
 
 
-	filetype = getFileType(args.file)
+	filetype = getFileType(file)
 
 	fileExtension = filetypeConversions(filetype)
 	outfileName = "outtathisworld." + fileExtension
@@ -240,7 +420,20 @@ def filetypeConversions(filetype):
 def getFileType (filename):
 
 	here = os.path.dirname(os.path.abspath(__file__))
-	filepath = here + "/" + filename
+
+	if args.folder:
+		#here = os.path.dirname(os.path.abspath(__file__))
+		print (args.folder)
+		print(type(args.folder))
+		print(here)
+		print(type(here))
+		print(filename)
+		print(type(filename))
+		filepath = here + "/" + args.folder + "/" + filename
+	else: 
+		filepath = here + "/" + filename
+	
+
 	filetype = imghdr.what(filepath)
 
 	return filetype 
@@ -257,8 +450,11 @@ def somefunction(outfiletype):
 #Run
 if __name__ == "__main__":
 
+	start = time.time()
+
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--file", help = "The file you want to denoise")
+	parser.add_argument("--folder", help = "The folder containing the files you want to denoise")
 	parser.add_argument("--lamb", help ="The lambda value for denoising", default = 0.1, type = float)
 	parser.add_argument('--outfiletype', help = "The output file type", type = somefunction)
 
@@ -267,46 +463,105 @@ if __name__ == "__main__":
 
 
 	#Due to argparse we should be able to remove this try.
-	try: 
-		newfile = input(args.file)
-		#exit(1)
-		#newfile = input(sys.argv[1])
+	if args.file: 
+		try: 
+			newfile = input(args.file)
+			#exit(1)
+			#newfile = input(sys.argv[1])
 
-	except IndexError:
-		print("You need to give us a file mate, otherwise how will I know what to denoise?")
-		print("Soon we will add the ability to use one of our test files as default!")
-		exit(1)
+		except IndexError:
+			print("You need to give us a file mate, otherwise how will I know what to denoise?")
+			print("Soon we will add the ability to use one of our test files as default!")
+			exit(1)
 
 
-	#image = Image.open("tempfile.png")
-	#image.show()	
-	#io.imshow(newfile)
+	#denoiseAutomatedPairWise(newfile)
+
+	#fileIndex = calculateBest()
+
+	#print(images[fileIndex])
+
+	#openFile(images[fileIndex])
+
+
+
+	#newblah = denoiseManual(newfile,0.1)
+
+	#showFile(newblah)
+
+	#end = time.time()
+
+	#noisyness = list(np.arange(0,0.15,0.001))
+
+
+
+	#print(metrics)
+	#plt.plot(metrics,'ro')
 	#plt.show()
-	#image = Image.open(args.file)
-	#image.show()
-
-	#So it is image.show option 
-
-	#print("HellloooOoo")
 
 
-	#print(newfile)
+
+	if args.folder: 
+		import os
+		arr = os.listdir('.')
+		print(arr)
+
+		filepath =  args.folder + '/'
+		print("The filepath is", filepath)
+
+		#You need to denoise the first file, 
+		#and then use that as a standard. 
+		#We need to check the first file though
+		#Maybe denoise a file in the middle - it should
+		#Theoretically have the most detail 
+		#Get the most accurate first reading 
+
+
+		for filename in os.listdir(filepath):
+			print filename
+			if filename.endswith(".png"):  
+				newfile = input(filename)
+				newDenoised = denoiseManual(newfile, 0.1)
+				showFile(newDenoised)
+	else:
+		#newDenoised = denoiseAutomatedPairWise(newfile)
+		#showFile(newDenoised)
+		#print("Hello")
+		denoiseAutomated(newfile)
+
+		fileIndex = calculateBest()
+
+
+		f = open('datafull.csv','a')
+
+		data = images[fileIndex] + '\t' + args.file + '\n'
+		f.write(data)
+		f.close()
+
+		print(images[fileIndex])
+
+		#print (metrics)
+
+		#plt.plot(metrics,'ro')
+		#plt.show()
+
+		#openFile(images[fileIndex])
+
+
+	#Check one file 
+
+
+	#Put all the files in one directory - Go through them. 
+	#Upload all the files onto the github 
+	#Create a form for Gavin and get him to tell you the best. 
 	
-	#io.imshow(newfile)
-
-	#io.show()
-
-	#outputFile = denoise(newfile)
 
 
-	#outputFile = denoiseManualIteration(newfile, args.lamb)
-	#print(outputFile)
-	#output(outputFile)
 
-	denoiseAutomated(newfile)
+	
+	#print('TOTAL TIME ONE IMAGE ' + str(end-start))
 
 
 
 #this link 
 #https://github.com/InsightSoftwareConsortium/DCMTK
-
